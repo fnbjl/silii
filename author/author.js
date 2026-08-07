385cef9e5dc7f2cc1e0f66e3376a89dc2522f3c0
fnbjl <1225234090@qq.com>
2026-08-08 00:15:22 +0800

    Update author.js

--- author/author.js
+++ author/author.js
@@ -1,1 +1,75 @@
 
+// 索引数据源，Python自动格式化生成
+window.groupData = {
+    "2": [
+        {
+            "name": "260429盲盒：",
+            "url": "https://pan.baidu.com/s/1uHaPfYSef3ez_CxG-zpyYg?pwd=sili"
+        }
+    ],
+    "A": [
+        {
+            "name": "260429神秘：A",
+            "url": "https://pan.baidu.com/s/1LDqoR1LvBE5lzP9wASJg9Q?pwd=sili"
+        },
+        {
+            "name": "ABO海棠1v1合集",
+            "url": "https://pan.baidu.com/s/18XL7aHWXFW-iPQPyB1InyA?pwd=sili"
+        },
+        {
+            "name": "ABO海棠合集",
+            "url": "https://pan.baidu.com/s/1jUatP97JbCKxAwag4rkRsw?pwd=sili"
+        }
+    ],
+    "B": [
+        {
+            "name": "260428加餐：BG快穿合集100本",
+            "url": "https://pan.baidu.com/s/12BCs6PsORfQZnTLvlmbcsQ?pwd=sili"
+        },
+        {
+            "name": "260429加餐：BG快穿合集100本",
+            "url": "https://pan.baidu.com/s/1tGoCD_3L7K4eOlTY-hvPMQ?pwd=sili"
+        },
+        {
+            "name": "BG白月光女主合集",
+            "url": "https://pan.baidu.com/s/1UK_AZGNmxmxpf2XRamzzFQ?pwd=sili"
+        },
+        {
+            "name": "BG扮乖女主合集",
+            "url": "https://pan.baidu.com/s/1u2mK2N-FI2pg3YM_B7xDIw?pwd=sili"
+        },
+        {
+            "name": "BG财迷女主合集",
+            "url": "https://pan.baidu.com/s/1lzXqnssy-8qCGTG7sQY9Vw?pwd=sili"
+        },
+        {
+            "name": "BG纯欲女主合集",
+            "url": "https://pan.baidu.com/s/1a3osx-8mUn5iZka6YoA0bQ?pwd=sili"
+        },
+        {
+            "name": "【四里】BG纯欲女主合集",
+            "url": "https://pan.baidu.com/s/1a3osx-8mUn5iZka6YoA1bQ?pwd=sili"
+        }
+    ],
+    "作": [
+        {
+            "name": "作者",
+            "url": "【百度】https://pan.baidu.com/s/1ddbROeX5ag7x0pwjBW6_xA?pwd=sili\n【迅雷】https://pan.xunlei.com/s/VOsMX7ZUs27wtfsSamzPRgoHA1?pwd=mycr\n【移动】https://yun.139.com/shareweb/#/w/i/2w2KK0hW3bwxt  提取码：dh3s\n【LZY百度】https://pan.baidu.com/s/1tn1i6f2C3yxsyjCjOpxzAA?pwd=sili\n【LZY夸克】https://pan.quark.cn/s/76051936a674\n【LZY迅雷】：https://pan.xunlei.com/s/VOxBXgIH_J7Q6cxfJy1KU8N0A1  提取码：etfm"
+        }
+    ],
+    "近": [
+        {
+            "name": "260426鸡肋：近期连载新文50本",
+            "url": "https://pan.baidu.com/s/1O5Mcg40IRCcp0X8eocu68w?pwd=sili"
+        },
+        {
+            "name": "260428鸡肋：近期连载新文50本",
+            "url": "https://pan.baidu.com/s/18_cbkQgwjhKAuoMyBiWrfw?pwd=sili"
+        },
+        {
+            "name": "260429鸡肋：近期连载新文50本",
+            "url": "https://pan.baidu.com/s/1H0MsHZzQpuqr7aqfMz7VWQ?pwd=sili"
+        }
+    ]
+};
+window.totalCount = 15;
