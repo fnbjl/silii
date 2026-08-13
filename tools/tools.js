e618aeebd7bf7cb6550344d97174353d3ad5910a
fnbjl <1225234090@qq.com>
2026-08-14 05:27:33 +0800

    Create tools.js
    创建tools.js
    更改tools.html样式逻辑

---
+++ tools/tools.js
@@ -0,0 +1,76 @@
+// tools.js
+const toolsData = [
+  {
+    title: "一、加速器",
+    items: [
+      {
+        label: "👉🏻汇总",
+        html: `<div>网址：<a href="https://sili.cc.cd">https://sili.cc.cd</a></div>`
+      },
+      {
+        label: "👉🏻解压专家",
+        html: `<div>下载链接：<a href="https://pan.baidu.com/s/1JMHp6vUoYZFEpTKk85Mpmg?pwd=sili">https://pan.baidu.com/s/1JMHp6vUoYZFEpTKk85Mpmg?pwd=sili</a></div>`
+      },
+      {
+        label: "👉🏻【备用链接：下面打不开点下面】✔",
+        html: `
+          <div class="desc">含以下安装包</div>
+          <div><a href="https://url46.ctfile.com/s/2b02971cf50feb61d3fcf0c6abb58ce0?p=123456">🔗直链下载地址：不显示换浏览器，密码123456</a></div>
+          <div><a href="https://pan.quark.cn/s/9378d5888f4b">🔗夸克下载地址：不要在线解压，会和谐</a></div>
+          <div>📮 <a href="node.html" target="_blank">免费节点丨Clash/V2Ray/SSR最新机场订阅</a></div>
+        `
+      },
+      {
+        label: "👉🏻【旋风咖越器】✔",
+        html: `
+          <div class="label">(注册后填写邀请码：<span class="code">0DKLC7Q</span>)</div>
+          <div class="desc">👉🏻老牌子了，很稳定，邀请可以免费</div>
+          <div>下载链接：<a href="https://www.67553555.com/0DKLC7Q">https://www.67553555.com/0DKLC7Q</a></div>
+        `
+      },
+      {
+        label: "👉🏻【雷霆咖越器】✔",
+        html: `
+          <div class="label">(注册后填写邀请码：<span class="code">I84OBD</span>)</div>
+          <div class="desc">👉🏻老牌子了，很稳定，邀请可以免费</div>
+          <div>下载链接：<a href="https://www.67284687.com/i/I84OBD">https://www.67284687.com/i/I84OBD</a></div>
+        `
+      },
+      {
+        label: "👉🏻【黑洞咖越器】✔",
+        html: `
+          <div class="label">(注册后填写邀请码：<span class="code">ZZWIY0</span>)</div>
+          <div class="desc">👉🏻老牌子了，很稳定，邀请可以免费</div>
+          <div>下载链接：<a href="https://www.78457389.com/id/ZZWIY0">https://www.78457389.com/id/ZZWIY0</a></div>
+        `
+      },
+      {
+        label: "👉🏻【手机端】蓝云：蓝奏云批量下载工具",
+        html: `<div>下载链接：<a href="https://pan.baidu.com/s/1aXZJINyIlndGhrsPfvLa8w?pwd=sili#">https://pan.baidu.com/s/1aXZJINyIlndGhrsPfvLa8w?pwd=sili#</a></div>`
+      }
+    ]
+  },
+  {
+    title: "二、网站",
+    items: [
+      {
+        label: "👉🏻韩漫官网传送门",
+        html: `
+          <div><a href="shturl.cc/3bqsiTkqxlQs">shturl.cc/3bqsiTkqxlQs</a></div>
+          <div><a href="shturl.cc/klc1wHTx1er">shturl.cc/klc1wHTx1er</a></div>
+          <div>韩版 <a href="https://www.bomtoon.com/">https://www.bomtoon.com/</a></div>
+          <div>台版 <a href="https://www.bomtoon.tw/">https://www.bomtoon.tw/</a></div>
+        `
+      },
+      {
+        label: "👉🏻一些吃饭网站",
+        html: `
+          <div>P站：<a href="https://www.pixiv.net/">https://www.pixiv.net/</a> <span class="note">(需墙)</span></div>
+          <div>P站镜像：<a href="https://pixiviz.xyz/">https://pixiviz.xyz/</a> <span class="note">(需墙)</span></div>
+          <div>小说笔趣阁<a href="shturl.cc/y3N77nF5o">shturl.cc/y3N77nF5o</a></div>
+          <div>歌曲<a href="https://gqsou.com">https://gqsou.com</a></div>
+        `
+      }
+    ]
+  }
+];
